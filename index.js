391e091e66c7b137aab03ab76f9c90633c3fca5f
Benhin <benhinmwendwa7@gmail.com>
2024-04-05 01:25:00 +0300

    filter

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+const drivers=[`Bobby`,`Sammy`,`Sally`,`Annette`,`Sarah`,`Bobby`]
+function findMatching(drivers , name){
+    
+    const lowerCName=name.toLowerCase()
+    return drivers.filter(drivers=>drivers.toLowerCase()===lowerCName) 
+
+}
+const value = findMatching(drivers ,`Bobby`)
+console.log(value) 
+function   fuzzyMatch(drivers,letters)
+{
+return drivers.filter(drivers=>drivers.startsWith(letters))
+}
+const fuzzy= fuzzyMatch(drivers,`S`)
+console.log(fuzzy)
+function matchName(drivers,match){
+    
+        return drivers.filter((record) => record.name === match);
+      }
